5fd68b218e29e40218e4575179477e47a2d6530f
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-06-04 16:44:30 -0400

    Lab Complete

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,26 @@
 // Write your code here
+const products = [
+  'Laptop',
+  'Phone',
+  'Headphones',
+  'Monitor'
+];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(producteName) {
+  products.push(producteName);
+}
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+function removeLastProduct(){
+  products.pop();
+}
 
 
 // Export the necessary parts for testing
